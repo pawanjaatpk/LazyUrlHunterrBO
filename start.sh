2f3e9f988ab80013baa252163e2c5d073be38d23
pawanjaatpk <118508835+pawanjaatpk@users.noreply.github.com>
2023-04-22 17:39:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/pawanjaatpk/LazyUrlHunterrBOT.git /LazyUrlHunterrBOT
-cd /LazyUrlHunterrBOT
+git clone https://github.com/pawanjaatpk/LazyUrlHunterrBO.git /LazyUrlHunterrBO
+cd /LazyUrlHunterrBO
 pip3 install -U -r requirements.txt
 echo "⚡️⚡️ Starting LazyUrlHunterrBOT..."
 python3 main.py
